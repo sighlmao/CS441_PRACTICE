7abebadf1bc7ae561f02d4e929f3e02faa2c4674
samgi <samgiabao2019@gmail.com>
2024-10-05 16:05:16 +0700

    lan12

--- EX002/app/src/main/java/com/example/ex2/MainActivity.java
+++ EX002/app/src/main/java/com/example/ex2/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex2;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,59 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    Button btncall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Toast.makeText(this, "CR424 - onCreat()", Toast.LENGTH_SHORT).show();
+        btncall = findViewById(R.id.btncall);
+        btncall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+                Intent intent1 = new Intent(MainActivity.this, subactivity.class);
+                startActivity(intent1);
+            }
         });
+        
+        protected void onDestroy() {
+// TODO Auto-generated method stub
+            super.onDestroy();
+            Toast.makeText(this,"CR424 - on Destroy()",Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        protected void onPause() {
+// TODO Auto-generated method stub
+            super.onPause();
+            Toast.makeText(this," CR424 - onPause", Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        protected void onRestart() {
+// TODO Auto-generated method stub
+            super.onRestart();
+            Toast.makeText(this," CR424 - onRestart", Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        protected void onResume() {
+// TODO Auto-generated method stub
+            12super.onResume();
+            Toast.makeText(this," CR424 - onResume", Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        protected void onStart() {
+// TODO Auto-generated method stub
+            super.onStart();
+            Toast.makeText(this," CR424 - onStart", Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        protected void onStop() {
+// TODO Auto-generated method stub
+            super.onStop();
+            Toast.makeText(this," CR424 - onStop", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- EX002/app/src/main/java/com/example/ex2/subactivity.java
+++ EX002/app/src/main/java/com/example/ex2/subactivity.java
@@ -1,10 +1,10 @@
+package com.example.ex2;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.ex2.R;
 
 public class subactivity extends AppCompatActivity {
     Button btnok;
@@ -16,7 +16,7 @@ public class subactivity extends AppCompatActivity {
         btnok = findViewById(R.id.btnok);
         btnok.setOnClickListener(new View.OnClickListener() {
             @Override
-10public void onClick(View v) {
+public void onClick(View v) {
 // TODO Auto-generated method stub
                 finish();
             }
